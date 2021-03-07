c5fe4206cec41fc62810fdfae7900edfad2cd3ed
Ionut Eugen OANCEA <eoancea@bitdefender.com>
2021-03-07 16:25:36 +0200

    api base route handling & util functions

---
+++ utils/utils.js
@@ -0,0 +1,19 @@
+module.exports.innerText = (text) => {
+	var response = "";
+	text.split(/&lt;/).map((a) => (a.split(/&gt;/)[1] ? (response += a.split(/&gt;/)[1] + " ") : null));
+	return response;
+};
+module.exports.rssContent = (text) => {
+	var response = "";
+	text.split("</content>").map((a) => (a.split('<content type="html">')[1] ? (response += a.split('<content type="html">')[1] + " ") : null));
+	return response;
+};
+module.exports.queryParse = (req) => {
+	if (req.url.split(/\?/)[1]) {
+		var processed = {};
+		req.url.split(/\?/)[1].split(/&/).map((p) => (processed[p.split(/=/)[0]] = p.split(/=/)[1]));
+		req.query = processed;
+	}
+	req.url = req.url.split(/\?/)[0];
+    return req;
+};
